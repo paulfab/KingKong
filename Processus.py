9fffa315d2f63cdbd4de9a75cda468b47bfd4935
benallamar <marouan.benalla@gmail.com>
2016-02-11 18:51:39 +0100

    prepare the file parser

--- Processus.py
+++ Processus.py
@@ -1,6 +1,7 @@
 # encoding: utf-8
 from __future__ import unicode_literals
 from fileParser import FileParser
-file_name = "source/learn_and_teach.in"
+file_name = "source/busy_day.in"
 file_parser = FileParser(file_name)
 file_parser.init()
+print file_parser.row
--- fileParser.py
+++ fileParser.py
@@ -2,14 +2,15 @@ class FileParser(object):
     source = None
     def __init__(self,input = None):
         #here we set all the global variables that we need
-        self.n = 0
-        self.m = 0
+        self.row = 0
+        self.column = 0
+        self.drone = 0
+        self.turn = 0
+        self.max = 0
         if input:
             self.source = open(input,"r")
     def init(self):
-        self.n,self.m = self.source.readline().split(' ')
-        self.n = int(self.n)
-        self.m = int(self.m)
+        self.row,self.column,self.drone,self.turn,self.max = self.source.readline().split(' ')
     def optimise(self):
         #herer we will implement our optimisation process
         pass
